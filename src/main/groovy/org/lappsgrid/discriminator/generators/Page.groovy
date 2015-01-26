05bb412b0f5674e5af1b854005b236b59b51749d
Keith Suderman <suderman@anc.org>
2015-01-26 16:00:33 -0500

    First working version.

--- src/main/groovy/org/lappsgrid/discriminator/generators/Page.groovy
+++ src/main/groovy/org/lappsgrid/discriminator/generators/Page.groovy
@@ -3,6 +3,9 @@ package org.lappsgrid.discriminator.generators
 import org.lappsgrid.discriminator.dsl.DiscriminatorDelegate
 
 /**
+ * The information needed to generate a single html page for one or
+ * more related discriminator types. E.g. discriminators for licenses.
+ *
  * @author Keith Suderman
  */
 class Page implements Iterable<Info> {
@@ -10,8 +13,8 @@ class Page implements Iterable<Info> {
     String path
     List<Info> discriminators = []
 
-    void add(DiscriminatorDelegate discriminator, String uri, String fragement) {
-        discriminators << new Info(discriminator: discriminator, uri: uri, fragment: fragement)
+    void add(DiscriminatorDelegate discriminator, String uri, String fragment) {
+        discriminators << new Info(discriminator: discriminator, uri: uri, fragment: fragment)
     }
 
     Iterator<Info> iterator() {
